15ea595440cad399f885d0ff51f7ade36c9a882a
Oron Adam <oronoa@gmail.com>
2020-12-04 18:30:22 +0200

    df

--- scripts/build-postgres-with-wal-g-docker-image.sh
+++ scripts/build-postgres-with-wal-g-docker-image.sh
@@ -5,4 +5,4 @@ cd "$(dirname "$0")/../"
 
 ./scripts/build-wal-g-docker-image.sh
 
-docker build ./docker-image/postgres-with-wal-g -t stephaneklein/postgres-with-walg:12.2-alpine-walg0.2.15
+docker build ./docker-image/postgres-with-wal-g -t oronoa/postgres-with-walg:13.1-alpine-walg0.2.19
--- scripts/build-wal-g-docker-image.sh
+++ scripts/build-wal-g-docker-image.sh
@@ -3,4 +3,4 @@ set -e
 
 cd "$(dirname "$0")/../"
 
-docker build ./docker-image/wal-g/ -t stephaneklein/wal-g:v0.2.15-alpine3.11.5
+docker build ./docker-image/wal-g/ -t oronoa/wal-g:v0.2.19-alpine3.11.5
